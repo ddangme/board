d524c943d059d6837b111786f2a793a116f32c4c
ddangme <ssilver121@naver.com>
2024-01-14 14:36:13 +0900

    #11 - 도메인 코드를 인테티로 변환
    1 도메인 클래스 == 1테이블 매칭되도록 설계했다.
    생성자와 `equals()`, `hashcode()` 등이 도메인 맥락에 맞게 설정했다.

---
+++ src/main/java/com/ddangme/board/domain/ArticleComment.java
@@ -0,0 +1,60 @@
+package com.ddangme.board.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy"),
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class ArticleComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter @ManyToOne(optional = false) private Article article; // 게시글 (ID)
+    @Setter @Column(nullable = false, length = 500) private String content; // 본문
+
+    @CreatedDate
+    private LocalDateTime createdAt; // 생성 일시
+    @CreatedBy
+    @Column(nullable = false, length = 100) private String createdBy; // 생성자
+    @LastModifiedDate
+    private LocalDateTime modifiedAt; // 수정 일시
+    @LastModifiedBy
+    @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
+
+    public ArticleComment(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+
+    public static ArticleComment of(Article article, String content) {
+        return new ArticleComment(article, content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArticleComment that)) return false;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
