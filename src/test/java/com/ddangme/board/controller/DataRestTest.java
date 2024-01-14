abf918aaa09bef63f9cfcf6e21b6e425561dad29
ddangme <ssilver121@naver.com>
2024-01-14 21:19:14 +0900

    #6 - Spring Data REST 테스트 제외 처리
    이것은 이 프로젝트에서  비즈니스 로직으로 구현한 코드가 아닌, Data REST 기능이며, 통합테스트라 무거우며 DB에도 영향을 주기 때문에 제외처리한다.

--- src/test/java/com/ddangme/board/controller/DataRestTest.java
+++ src/test/java/com/ddangme/board/controller/DataRestTest.java
@@ -1,5 +1,6 @@
 package com.ddangme.board.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Disabled("Spring Data REST 통합 테스트는 불필요하기 때문에 제외한다.")
 @DisplayName("Data REST API TEST")
 @AutoConfigureMockMvc
 @Transactional
