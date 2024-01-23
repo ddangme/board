6eea260413998cdacda1511d0d97bce0779d5844
ddangme <ssilver121@naver.com>
2024-01-23 16:26:38 +0900

    feat: 게시판  View 기본 골격 작성 (#16)

--- src/test/java/com/ddangme/board/controller/ArticleControllerTest.java
+++ src/test/java/com/ddangme/board/controller/ArticleControllerTest.java
@@ -21,7 +21,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -31,7 +30,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles"))   // "/articles"의 경로에 대한 GET 요청
                 // andExpect: "MockMvcResultMatchers" 클래스의 메서드로, 컨트롤러가 생성한 응답을 검증한다.
                 .andExpect(status().isOk())        // HTTP 응답 상태 코드가 "200 OK" 인지 확인한다.
-                .andExpect(content().contentType(MediaType.TEXT_HTML))      // 응답의 콘텐츠 타입이 "text/html" 인지 확인한다.
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))      // 응답의 콘텐츠 타입이 "text/html" 인지 확인한다.
                 .andExpect(view().name("articles/index"))  // 응답되는 페이지의 경로가 "articles/index" 인지 확인한다.
                 .andExpect(model().attributeExists("articles"));    // 모델에 "articles" 라는 속성이 존재하는지 확인한다. "articles"라는 이름의 모델 속성을 뷰에 존달하는지 확인
     }
