c96bc51428fd58acebea1ac3c3795f61c82fd1ad
ddangme <ssilver121@naver.com>
2024-01-23 23:50:16 +0900

    feat: user_account entity 추가 (#17)

--- src/main/java/com/ddangme/board/domain/ArticleComment.java
+++ src/main/java/com/ddangme/board/domain/ArticleComment.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy"),
@@ -19,15 +19,20 @@ public class ArticleComment extends AuditingFields {
     private Long id;
 
     @Setter @ManyToOne(optional = false) private Article article; // 게시글 (ID)
+
+    @Setter @ManyToOne(optional = false)
+    private UserAccount userAccount;
+
     @Setter @Column(nullable = false, length = 500) private String content; // 본문
 
-    public ArticleComment(Article article, String content) {
+    public ArticleComment(Article article, UserAccount userAccount, String content) {
         this.article = article;
+        this.userAccount = userAccount;
         this.content = content;
     }
 
-    public static ArticleComment of(Article article, String content) {
-        return new ArticleComment(article, content);
+    public static ArticleComment of(Article article, UserAccount userAccount, String content) {
+        return new ArticleComment(article, userAccount, content);
     }
 
     @Override
