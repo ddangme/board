b05cd4837610932a453831932a9b0beb916b5ce5
ddangme <ssilver121@naver.com>
2024-01-24 15:21:37 +0900

    docs: 도메인 클래스 수정 (#17)

--- src/main/java/com/ddangme/board/domain/Article.java
+++ src/main/java/com/ddangme/board/domain/Article.java
@@ -5,7 +5,6 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
-import javax.persistence.Table;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -16,34 +15,29 @@ import java.util.Set;
         @Index(columnList = "title"),
         @Index(columnList = "hashtag"),
         @Index(columnList = "createdAt"),
-        @Index(columnList = "createdBy"),
+        @Index(columnList = "createdBy")
 })
 @Entity
 public class Article extends AuditingFields {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Setter
-    @ManyToOne(optional = false)
-    private UserAccount userAccount;
+    @Setter @ManyToOne(optional = false) private UserAccount userAccount; // 유저 정보 (ID)
 
-    @Setter
-    @Column(nullable = false)
-    private String title; // 제목
-    @Setter
-    @Column(nullable = false, length = 10000)
-    private String content; // 본문
-    @Setter
-    private String hashtag; // 해시태그
+    @Setter @Column(nullable = false) private String title; // 제목
+    @Setter @Column(nullable = false, length = 10000) private String content; // 본문
+
+    @Setter private String hashtag; // 해시태그
 
     @ToString.Exclude
     @OrderBy("createdAt DESC")
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
-    protected Article() {
-    }
+
+    protected Article() {}
 
     private Article(UserAccount userAccount, String title, String content, String hashtag) {
         this.userAccount = userAccount;
@@ -60,11 +54,12 @@ public class Article extends AuditingFields {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Article article)) return false;
-        return id != null && Objects.equals(id, article.id);
+        return id != null && id.equals(article.id);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(id);
     }
+
 }
