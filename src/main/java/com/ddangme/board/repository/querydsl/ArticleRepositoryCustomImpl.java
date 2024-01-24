f377d4517af3c3f317c2421405001ce499fd24c3
ddangme <ssilver121@naver.com>
2024-01-25 00:37:34 +0900

    feat: 해시태그 페이지 기본 기능 추가

---
+++ src/main/java/com/ddangme/board/repository/querydsl/ArticleRepositoryCustomImpl.java
@@ -0,0 +1,24 @@
+package com.ddangme.board.repository.querydsl;
+
+import com.ddangme.board.domain.Article;
+import com.ddangme.board.domain.QArticle;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.Collection;
+import java.util.List;
+
+public class ArticleRepositoryCustomImpl extends QuerydslRepositorySupport implements ArticleRepositoryCustom {
+    public ArticleRepositoryCustomImpl() {
+        super(Article.class);
+    }
+
+    @Override
+    public List<String> findAllDistinctHashtags() {
+        QArticle article = QArticle.article;
+
+        return from(article)
+                .distinct()
+                .select(article.hashtag)
+                .fetch();
+    }
+}
