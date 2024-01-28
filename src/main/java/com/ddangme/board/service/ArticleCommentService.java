69ca3e2c73fd243c11570cbbabf126715da6a86f
ddangme <ssilver121@naver.com>
2024-01-28 17:20:35 +0900

    refactor: 코드 정렬 정리 및 코드 설명 주석 추가

--- src/main/java/com/ddangme/board/service/ArticleCommentService.java
+++ src/main/java/com/ddangme/board/service/ArticleCommentService.java
@@ -43,6 +43,10 @@ public class ArticleCommentService {
         }
     }
 
+    /**
+     * @deprecated 댓글 수정 기능은 클라이언트에서 생각할 점이 많아지기 때문에, 이번 강의에서는 제공하지 않기로 하였다.
+     */
+    @Deprecated
     public void updateArticleComment(ArticleCommentDto dto) {
         try {
             ArticleComment articleComment = articleCommentRepository.getReferenceById(dto.id());
