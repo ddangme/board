97d4cc96950f9f3013bfec99acec41a4c6d8be1b
ddangme <ssilver121@naver.com>
2024-01-29 23:54:11 +0900

    feat: OAuth 인증 정보를 다룰 수 있도록 BoardPrincipal 수정
    #52

--- src/main/java/com/ddangme/board/dto/security/BoardPrincipal.java
+++ src/main/java/com/ddangme/board/dto/security/BoardPrincipal.java
@@ -5,22 +5,29 @@ import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public record BoardPrincipal(
         String username,
         String password,
-        Collection<? extends GrantedAuthority> authrities,
+        Collection<? extends GrantedAuthority> authorities,
         String email,
         String nickname,
-        String memo
-) implements UserDetails {
+        String memo,
+        Map<String, Object> oAuth2Attributes
+) implements UserDetails, OAuth2User {
 
     public static BoardPrincipal of(String username, String password, String email, String nickname, String memo) {
-        // 지금은 인증만 하고 권한을 다루고 있지 않기 때문에 임의로 세팅한 것이다.
+        return of(username, password, email, nickname, memo, Map.of());
+    }
+
+    public static BoardPrincipal of(String username, String password, String email, String nickname, String memo, Map<String, Object> oAuth2Attributes) {
+        // 지금은 인증만 하고 권한을 다루고 있지 않아서 임의로 세팅한다.
         Set<RoleType> roleTypes = Set.of(RoleType.USER);
 
         return new BoardPrincipal(
@@ -29,10 +36,12 @@ public record BoardPrincipal(
                 roleTypes.stream()
                         .map(RoleType::getName)
                         .map(SimpleGrantedAuthority::new)
-                        .collect(Collectors.toUnmodifiableSet()),
+                        .collect(Collectors.toUnmodifiableSet())
+                ,
                 email,
                 nickname,
-                memo
+                memo,
+                oAuth2Attributes
         );
     }
 
@@ -56,49 +65,36 @@ public record BoardPrincipal(
         );
     }
 
-    @Override
-    public String getPassword() {
-        return password;
+
+    @Override public String getUsername() { return username; }
+    @Override public String getPassword() { return password; }
+    @Override public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
     }
 
-    @Override
-    public String getUsername() {
+    @Override public boolean isAccountNonExpired() { return true; }
+    @Override public boolean isAccountNonLocked() { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled() { return true; }
+
+    // OAuth2 인증 관련
+    @Override public Map<String, Object> getAttributes() {
+        return oAuth2Attributes;
+    }
+
+    // OAuth2 인증 관련
+    @Override public String getName() {
         return username;
     }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authrities;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-
     public enum RoleType {
-        USER ("ROLE_USER");
+        USER("ROLE_USER");
 
-        @Getter
-        private final String name;
+        @Getter private final String name;
 
         RoleType(String name) {
             this.name = name;
         }
     }
+
 }
