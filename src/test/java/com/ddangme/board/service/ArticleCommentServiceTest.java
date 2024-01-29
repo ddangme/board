ef0f7c25550fe6cb7ba3788eaf7648fe1167b784
ddangme <ssilver121@naver.com>
2024-01-30 00:09:30 +0900

    Revert "[고도화] 댓글 기능 고도화 - 댓글 기능의 결정과 롤백"

--- src/test/java/com/ddangme/board/service/ArticleCommentServiceTest.java
+++ src/test/java/com/ddangme/board/service/ArticleCommentServiceTest.java
@@ -9,14 +9,12 @@ import com.ddangme.board.dto.UserAccountDto;
 import com.ddangme.board.repository.ArticleCommentRepository;
 import com.ddangme.board.repository.ArticleRepository;
 import com.ddangme.board.repository.UserAccountRepository;
-import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
 
 import javax.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
@@ -42,28 +40,16 @@ class ArticleCommentServiceTest {
     void givenArticleId_whenSearchingArticleComments_thenReturnsArticleComments() {
         // Given
         Long articleId = 1L;
-        ArticleComment expectedParentComment = createArticleComment(1L, "parent content");
-        ArticleComment expectedChildComment = createArticleComment(2L, "child content");
-
-        expectedChildComment .setParentCommentId(expectedParentComment.getId());
-
-        given(articleCommentRepository.findByArticle_Id(articleId)).willReturn(List.of(
-                expectedParentComment,
-                expectedChildComment
-        ));
+        ArticleComment expected = createArticleComment("content");
+        given(articleCommentRepository.findByArticle_Id(articleId)).willReturn(List.of(expected));
 
         // When
         List<ArticleCommentDto> actual = sut.searchArticleComments(articleId);
 
         // Then
-        assertThat(actual).hasSize(2);
         assertThat(actual)
-                .extracting("id", "articleId", "parentCommentId", "content")
-                .containsExactlyInAnyOrder(
-                        Tuple.tuple(1L, 1L, null, "parent content"),
-                        Tuple.tuple(2L, 1L, 1L, "child content")
-                );
-
+                .hasSize(1)
+                .first().hasFieldOrPropertyWithValue("content", expected.getContent());
         then(articleCommentRepository).should().findByArticle_Id(articleId);
     }
 
@@ -82,7 +68,6 @@ class ArticleCommentServiceTest {
         // Then
         then(articleRepository).should().getReferenceById(dto.articleId());
         then(userAccountRepository).should().getReferenceById(dto.userAccountDto().userId());
-        then(articleCommentRepository).should(never()).getReferenceById(anyLong()); // never()를 통해 호출된적이 없는지 확인한다.
         then(articleCommentRepository).should().save(any(ArticleComment.class));
     }
 
@@ -102,36 +87,38 @@ class ArticleCommentServiceTest {
         then(articleCommentRepository).shouldHaveNoInteractions();
     }
 
-    @DisplayName("부모 댓글 ID와 댓글 정보를 입력하면, 대댓글을 저장한다.")
+    @DisplayName("댓글 정보를 입력하면, 댓글을 수정한다.")
     @Test
-    void givenParentCommentIdAndArticleCommentInfo_whenSaving_thenSavesChildComment() {
+    void givenArticleCommentInfo_whenUpdatingArticleComment_thenUpdatesArticleComment() {
         // Given
-        Long parentCommentId = 1L;
-        ArticleComment parent = createArticleComment(parentCommentId, "댓글"); // 여기서 사용된 parentCommentId는 해당 parent 객체의 id로 지정된다.
-        ArticleCommentDto child = createArticleCommentDto(parentCommentId, "대댓글");
-
-        /*
-           given(): Mockito에서 해당 메서드가 호출될 때 어떤 동작을 수행하도록 정의한다.
-           예를 들면, 'given(articleRepository.getReferenceById(child.articleId()))'은
-           'aritlceRepository'의 'ㅎetReferenceById' 메서드가 특정 'child.articleId()' 매개변수로 호출될 때 어떤 값을 반환할지 정의한다.
-           즉, 'child.articleId()'에 대응하는 가짜(mock) 객체나 값을 반환하도록 설정한다.
-         */
-        given(articleRepository.getReferenceById(child.articleId())).willReturn(createArticle());
-        given(userAccountRepository.getReferenceById(child.userAccountDto().userId())).willReturn(createUserAccount());
-        given(articleCommentRepository.getReferenceById(child.parentCommentId())).willReturn(parent);
+        String oldContent = "content";
+        String updatedContent = "댓글";
+        ArticleComment articleComment = createArticleComment(oldContent);
+        ArticleCommentDto dto = createArticleCommentDto(updatedContent);
+        given(articleCommentRepository.getReferenceById(dto.id())).willReturn(articleComment);
 
         // When
-        sut.saveArticleComment(child);
+        sut.updateArticleComment(dto);
 
         // Then
-        assertThat(child.parentCommentId()).isNotNull();
-        /*
-           then(): Mockito에서 해당 메서드가 호출되었는지 여부를 검증하는 역할을 한다.
-         */
-        then(articleRepository).should().getReferenceById(child.articleId());
-        then(userAccountRepository).should().getReferenceById(child.userAccountDto().userId());
-        then(articleCommentRepository).should().getReferenceById(child.parentCommentId());
-        then(articleCommentRepository).should(never()).save(any(ArticleComment.class));
+        assertThat(articleComment.getContent())
+                .isNotEqualTo(oldContent)
+                .isEqualTo(updatedContent);
+        then(articleCommentRepository).should().getReferenceById(dto.id());
+    }
+
+    @DisplayName("없는 댓글 정보를 수정하려고 하면, 경고 로그를 찍고 아무 것도 안 한다.")
+    @Test
+    void givenNonexistentArticleComment_whenUpdatingArticleComment_thenLogsWarningAndDoesNothing() {
+        // Given
+        ArticleCommentDto dto = createArticleCommentDto("댓글");
+        given(articleCommentRepository.getReferenceById(dto.id())).willThrow(EntityNotFoundException.class);
+
+        // When
+        sut.updateArticleComment(dto);
+
+        // Then
+        then(articleCommentRepository).should().getReferenceById(dto.id());
     }
 
     @DisplayName("댓글 ID를 입력하면, 댓글을 삭제한다.")
@@ -149,20 +136,12 @@ class ArticleCommentServiceTest {
         then(articleCommentRepository).should().deleteByIdAndUserAccount_UserId(articleCommentId, userId);
     }
 
+
     private ArticleCommentDto createArticleCommentDto(String content) {
-        return createArticleCommentDto(null, content);
-    }
-
-    private ArticleCommentDto createArticleCommentDto(Long parentCommentId, String content) {
-        return createArticleCommentDto(1L, parentCommentId, content);
-    }
-
-    private ArticleCommentDto createArticleCommentDto(Long id, Long parentCommentId, String content) {
         return ArticleCommentDto.of(
-                id,
+                1L,
                 1L,
                 createUserAccountDto(),
-                parentCommentId,
                 content,
                 LocalDateTime.now(),
                 "uno",
@@ -185,21 +164,12 @@ class ArticleCommentServiceTest {
         );
     }
 
-    private ArticleComment createArticleComment(Long id, String content) {
-        ArticleComment articleComment = ArticleComment.of(
+    private ArticleComment createArticleComment(String content) {
+        return ArticleComment.of(
                 createArticle(),
                 createUserAccount(),
                 content
         );
-
-        /*
-           Reflection을 사용하여 테스트 코드에서 객체의 프라이빗 필드에 값을 설정할 때 사용한다.
-           이를 통해 테스트 코드에서는 일반적으로 접근할 수 없는 private이나 protected로 선언된 필드에도 값을 주입할 수 있다.
-           매개변수: 값을 설정하려는 대상 객체, 설정하려는 필드명, 설정하려는 값
-         */
-        ReflectionTestUtils.setField(articleComment, "id", id);
-
-        return articleComment;
     }
 
     private UserAccount createUserAccount() {
@@ -218,7 +188,6 @@ class ArticleCommentServiceTest {
                 "title",
                 "content"
         );
-        ReflectionTestUtils.setField(article, "id", 1L);
         article.addHashtags(Set.of(createHashtag(article)));
 
         return article;
